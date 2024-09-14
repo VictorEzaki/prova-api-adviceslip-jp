4c124aed331d8e7a9599f9851e72a0f01f60821e
Victor <victorezaki19@gmail.com>
2024-09-13 21:00:06 -0300

    feat: Page home finalizada

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,9 @@
 import './styles.css'
 
 export default function Footer (){
-
+    return(
+        <footer id='footer'>
+            <span>&copy; Todos os direitos reservados por Victor e Wanderson</span>
+        </footer>
+    )
 }
