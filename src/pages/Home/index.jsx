86c6108e6b82e4754f91247fa51bcf07f83e6949
Wandih <wander.ricardo967@gmail.com>
2024-09-14 10:28:31 -0300

    feat: atualizando a home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,12 @@ export default function Home (){
             <p id='main-paragraph'>
                 A API "Advice Slip" é uma API bastante simples e útil para obter conselhos aleatórios. Ela fornece uma interface para acessar uma coleção de dicas e conselhos em formato de texto.
             </p>
+            <p id='main-paragraph'>
+                Ao acessar a página "API", você será redirecionado para uma página com o subtítulo "Conselho do Dia" e com um conselho aleatório automaticamente gerado abaixo. 
+            </p>
+            <p id='main-paragraph'>
+                Ao clicar no botão "Obter novo conselho", você também pode trocar de conselho quantas vezes você quiser.
+            </p>
         </div>
     )
 }
